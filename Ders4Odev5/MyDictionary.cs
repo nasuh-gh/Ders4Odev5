d15855b31aef19eb8ec94d36b19ccce3d7c7ab81
nasuh-gh <nasuhguler@gmail.com>
2021-01-23 21:22:49 +0300

    Add project files.

---
+++ Ders4Odev5/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ders4Odev5
+{
+    class MyDictionary<R, T>
+    {
+        R[] renkler;
+        T[] items;
+        public MyDictionary()//Constructor-Class'ı new'lersen burası çalışıyor
+        {
+            renkler = new R[0];
+            items = new T[0];
+        }
+
+        public void Add(R renk, T item)
+        {
+            R[] tempRenk = renkler;
+            T[] tempArray = items;//geçici olarak referans numarasını burada tutturuyoruz
+            renkler = new R[renkler.Length + 1];
+            items = new T[items.Length + 1];//yukarıdakini yapmasaydık önceki verilerim kaybolacaktı
+            for (int i = 0; i < tempArray.Length; i++)//geçici emanet olarak verdiğim elemanları geri alacağım
+            {
+                renkler[i] = tempRenk[i];
+                items[i] = tempArray[i];
+            }
+            renkler[renkler.Length - 1] = renk;
+            items[items.Length - 1] = item;//burada add'le gelen yeni elemanı boş kısma ekliyoruz(getiriyoruz)
+            //length -1 toplam eleman sayısından 1 eksik yani index değeri
+
+            Console.WriteLine("Renk: "+renk + " Takım: " + item);
+
+        }
+    }
+}
---
+++ Ders4Odev5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ders4Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> takimlar = new MyDictionary<string,string>();
+
+            takimlar.Add("Sarı-Kırmızı","Galatasaray");
+            takimlar.Add("Sarı-Lacivert","Fenerbahçe");
+            takimlar.Add("Siyah-Beyaz", "Beşiktaş");
+
+        }
+    }
+}
